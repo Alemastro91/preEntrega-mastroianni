ca9ecd8e7aad06ec738ec64dd0b11158109f1a96
Alejandro <alemastro91inc@gmail.com>
2023-07-10 10:48:32 -0300

    creando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
-import './App.css'
+import CartWidget from './componentes/cartWidget'
+import ItemListContainer from './componentes/itemListCont'
 
 function App() {
   return (
-    <>
+    <div className = "App">
+      <CartWidget/>
+
+      <ItemListContainer/>
+
       <h1>Hola Mundo</h1>
-    </>
+
+    </div>
   )
 }
 
---
+++ src/componentes/cartWidget/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+import React from "react"
+import { BsCart4 } from "react-icons/Bs"
+
+const CartWidget = () => {
+    return (
+        <BsCart4 className = "cart-icon" /> //Dar props o calses al icon
+    )
+}
+
+export default CartWidget
